0c8bacaf85a8ef81bdbe40c909a1064017b6bfb9
Azamat <azamat_sizhazhev@mail.ru>
2023-03-12 12:49:45 +0300

    №6 Тестирование strip_punctuation_ru()

--- main.py
+++ main.py
@@ -1,49 +1,19 @@
-from yandex_testing_lesson import is_correct_mobile_phone_number_ru
+from yandex_testing_lesson import strip_punctuation_ru
 
 
-def test_is_correct_mobile_phone_number_ru():
+def test_strip_punctuation_ru():
     test_data = (
-        ('+79674129902', True),
-        ('89674129902', True),
-        ('+7(967)4129902', True),
-        ('8(967)4129902', True),
-        ('+7 967 412-99-02', True),
-        ('8 967 412-99-02', True),
-        ('+7 (967) 412-99-02', True),
-        ('8 (967) 412-99-02', True),
-
-        ('+796741299020', False),
-        ('896741299020', False),
-        ('+7(967)41299020', False),
-        ('8(967)41299020', False),
-        ('+7 967 412-99-020', False),
-        ('8 967 412-99-020', False),
-        ('+7 (967) 412-99-020', False),
-        ('8 (967) 412-99-020', False),
-
-        ('+796(7412)9902', False),
-        ('89)67412990(2', False),
-        ('+7)967(4129902', False),
-        ('8()9674129902', False),
-        ('+7 9(67 412-99)-02', False),
-        ('8 9)67 412-99(-02', False),
-        ('+7 (9)67) 412-(99-02', False),
-        ('8 (96(7) 412-99-)02', False),
-
-        ('+19674129902', False),
-        ('29674129902', False),
-        ('+3(967)4129902', False),
-        ('4(967)4129902', False),
-        ('+5 967 412-99-02', False),
-        ('6 967 412-99-02', False),
-        ('+9 (967) 412-99-02', False),
-        ('0 (967) 412-99-02', False)
+        ('Ваш коллега написал функцию, которая удаляет знаки препинания из передаваемой в нее строки',
+         'Ваш коллега написал функцию которая удаляет знаки препинания из передаваемой в нее строки'),
+        ('Подсказка - кое-какие слова в русском языке состоят не только из букв, и такое слово есть в этой фразе.',
+         'Подсказка кое-какие слова в русском языке состоят не только из букв и такое слово есть в этой фразе'),
+        ('''!()—[]{};:'"\,<>./?@#$%^&*_~''', '')
     )
 
     for input_data, correct_output_data in test_data:
-        if is_correct_mobile_phone_number_ru(input_data) != correct_output_data:
+        if strip_punctuation_ru(input_data) != correct_output_data:
             return False
     return True
 
 
-print('YES' if test_is_correct_mobile_phone_number_ru() else 'NO')
+print('YES' if test_strip_punctuation_ru() else 'NO')
--- yandex_testing_lesson.py
+++ yandex_testing_lesson.py
@@ -1,26 +1,27 @@
-import re
-import sys
+# Тест №1
+def strip_punctuation_ru(s):
+    punctuations = '''!()—[]{};:'"\,<>./?@#$%^&*_~'''
 
+    new_s = ""
+    for char in s:
+        if char in punctuations:
+            new_s += ' '
+        else:
+            new_s += char
 
-def is_correct_mobile_phone_number_ru(s):
-    remainder = ''
-    if s.startswith('+7'):
-        remainder = s[2:]
-    elif s.startswith('8'):
-        remainder = s[1:]
-    else:
-        return False
+    # заменим все последовательности вида " - " на " "
+    new_s = new_s.replace(" - ", " ")
+    return " ".join(new_s.split())
 
-    remainder = re.sub(r'[ -]', '', remainder)
-
-    if re.match(r'^\(\d{3}\)', remainder):
-        remainder = re.sub(r'\(', '', remainder, 1)
-        remainder = re.sub(r'\)', '', remainder, 1)
-
-    return bool(re.match(r'^\d{10}$', remainder))
-
-
-sys.modules['inspect'] = None
-
-
-print('YES' if is_correct_mobile_phone_number_ru(input()) else 'NO')
+# Тест №5
+# def strip_punctuation_ru(s):
+#     punctuations = '''!()—[]{};:'"\,<>./?@#$%^&*_~'''
+#
+#     new_s = ""
+#     for char in s:
+#         if char in punctuations:
+#             new_s += ' '
+#         else:
+#             new_s += char
+#
+#     return " ".join(new_s.split())
