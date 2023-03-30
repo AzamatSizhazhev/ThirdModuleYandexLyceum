1020bb52d20baf693d3a4754d7330f1547c25a26
Azamat <azamat_sizhazhev@mail.ru>
2023-03-30 17:44:55 +0300

    №4 Москвичи

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
+import json
 from zipfile import ZipFile
 
-
-def human_read_format(size):
-    names = ['Б', 'КБ', 'МБ', 'ГБ']
-    counter = 0
-    while size >= 1024:
-        size /= 1024
-        counter += 1
-    return f'{round(size)}{names[counter]}'
-
-
+counter = 0
 with ZipFile('input.zip') as myzip:
-    for elem in myzip.filelist:
-        if elem.filename[-1] == '/':
-            print('  ' * (elem.filename.count('/') - 1) + elem.filename.split('/')[-2])
-        else:
-            print('  ' * elem.filename.count('/') + elem.filename.split('/')[-1] + ' ' + human_read_format(
-                elem.file_size))
+    data = myzip.namelist()
+    for elem in data:
+        if elem[-1] != '/' and '.json' in elem:
+            with myzip.open(elem) as file:
+                info = json.load(file)
+            if info['city'] == 'Москва':
+                counter += 1
+print(counter)
