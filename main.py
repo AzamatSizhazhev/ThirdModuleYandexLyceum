03db62bc310b3a1b0c02d22e465744d476c94d2f
Azamat <azamat_sizhazhev@mail.ru>
2023-04-20 21:37:15 +0300

    №5 Слайд-шоу

--- main.py
+++ main.py
@@ -1,10 +1,74 @@
+import os
+import sys
+
+import pygame
 import requests
 
-list_of_cities = input().split(', ')
-positions = dict()
-for city in list_of_cities:
-    geocoder_request = f'http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={city}&format=json'
-    response = requests.get(geocoder_request)
-    json_response = response.json()
-    positions[city] = json_response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['boundedBy']['Envelope']['lowerCorner'].split()[-1]
-print(min(positions, key=lambda unit: float(positions[unit])))
+map_files = list()
+coordinates = tuple()
+map_requests = list()
+
+
+def make_maps(*args):
+    global map_files, coordinates, map_requests
+
+    coordinates = args
+    map_requests = [f'http://static-maps.yandex.ru/1.x/?ll={coordinates[0][1]},{coordinates[0][0]}&spn=0.004,0.004&l=sat',
+                    f'http://static-maps.yandex.ru/1.x/?ll={coordinates[1][1]},{coordinates[1][0]}&spn=0.004,0.004&l=sat',
+                    f'http://static-maps.yandex.ru/1.x/?ll={coordinates[2][1]},{coordinates[2][0]}&spn=0.004,0.004&l=sat']
+
+    for index, elem in enumerate(map_requests):
+        response = requests.get(elem)
+
+        if not response:
+            print('Ошибка выполнения запроса')
+            print(elem)
+            print('Http статус', response.status_code, '(', response.reason, ')')
+            sys.exit('Не повезло, не фортануло')
+
+        map_files.append(f'map{index}.png')
+        with open(map_files[-1], 'wb') as file:
+            file.write(response.content)
+
+
+def main():
+    make_maps((55.70311, 37.530887), (59.940082, 30.312814), (38.889922, -77.009927))
+
+    pygame.init()
+    size = 600, 480
+    screen = pygame.display.set_mode(size)
+
+    screen.fill((200, 200, 200))
+    font = pygame.font.Font(None, 20)
+    text = font.render(map_requests[0], True, (50, 50, 50))
+    screen.blit(text, (5, 5))
+
+    screen.blit(pygame.image.load(map_files[0]), (0, 25))
+    pygame.display.flip()
+
+    ind = 0
+    running = True
+    clock = pygame.time.Clock()
+    fps = 60
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                ind += 1
+                if ind >= len(coordinates):
+                    ind = 0
+                screen.fill((200, 200, 200))
+                text = font.render(map_requests[ind], True, (50, 50, 50))
+                screen.blit(text, (5, 5))
+                screen.blit(pygame.image.load(map_files[ind]), (0, 25))
+        clock.tick(fps)
+        pygame.display.flip()
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
+    print(map_requests[0])
+    for elem in map_files:
+        os.remove(elem)
